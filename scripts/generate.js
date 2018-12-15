ce0c67a69596742cb9ef3fc387a10e2c34cce008
Robin Heggelund Hansen <robin.heggelund@icloud.com>
2018-12-15 12:05:24 +0100

    Convert xml to json.

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,3 +1,19 @@
 const xml2js = require ('xml2js');
+const fs = require('fs');
+const util = require('util');
 
-console.log('TODO');
+const dbString = fs.readFileSync('phone_db.xml', 'utf-8');
+
+const parseOptions = {
+  trim: true,
+  explicitRoot: false
+};
+
+xml2js.parseString(dbString, parseOptions, (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(util.inspect(result, false, null));
+});
